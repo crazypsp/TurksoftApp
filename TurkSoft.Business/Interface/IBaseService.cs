8de93b6887bbbc38652f409468e6c14ec22258d7
Yusuf OZER <yusuf_ozer16@hotmail.com>
2025-07-29 17:32:14 +0300

    İş Katmanı tamamlandı

---
+++ TurkSoft.Business/Interface/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurkSoft.Business.Interface
+{
+    public interface IBaseService<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
---
+++ TurkSoft.Business/Managers/BaseManager.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TurkSoft.Data.Context;
+using TurkSoft.Entities;
+using TurkSoft.Business.Interface;
+
+namespace TurkSoft.Business.Managers
+{
+    public class BaseManager<T> : IBaseService<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseManager(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var entity=await _dbSet.FindAsync(id);
+            if(entity==null)return false;
+            entity.IsActive = false;
+            entity.DeleteDate=DateTime.Now;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet
+                .AsNoTracking()
+                .Where(x => x.IsActive)
+                .ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsActive);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            entity.UpdateDate = DateTime.Now;
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
