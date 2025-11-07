d69a3ca2eef10b68ffd1d106abdf0ba5ed517c7e
Yusuf OZER <yusuf_ozer16@hotmail.com>
2025-11-07 16:39:29 +0300

    DB işlemi Tamamdır.

--- TurkSoft.Data/GibData/Migrations/20251107095826_SyncModel_20251107.Designer.cs
+++ TurkSoft.Data/GibData/Migrations/20251107095826_SyncModel_20251107.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TurkSoft.Data.GibData;
 
@@ -11,9 +12,11 @@ using TurkSoft.Data.GibData;
 namespace TurkSoft.Data.GibData.Migrations
 {
     [DbContext(typeof(GibAppDbContext))]
-    partial class GibAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251107095826_SyncModel_20251107")]
+    partial class SyncModel_20251107
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
